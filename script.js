f96061f2d574d54370931dc302b6118cd2a102d0
Ricardo Borges <ricardoborges00@hotmail.com>
2022-07-11 23:02:20 -0300

    finalizado

---
+++ script.js
@@ -0,0 +1,55 @@
+let comedy = [{
+    title: 'Just Go With It',
+    Director: 'Dennis Dugan',
+    year: 2011,
+},
+{
+    title: 'Click',
+    Director: ' Frank Coraci',
+    year: 2006, 
+},
+{
+    title: 'good boys',
+    Director: 'James Nunn',
+    year: 2022,
+}]
+
+let action = [{
+    title: 'Uncut Gems',
+    Director: ' Josh Safdie, Benny Safdie',
+    year: 2019,
+},
+{
+    title: 'Nobody ',
+    Director: ' Ilya Naishuller',
+    year:2021,
+},
+{
+    title: 'Promising Young Woman',
+    Director: 'Emerald Fennell',
+    year: 2021,
+}]
+
+let fantasy = [{
+    title: 'The Lord of the Rings: The Return of the King',
+    Director: 'Peter Jackson',
+    year: 2003,
+},
+{
+    title: 'Harry Potter and the Order of the Phoenix',
+    Director: 'David Yates',
+    year: 2007,
+},
+{
+    title: 'Pans Labyrinth',
+    Director: 'Guillermo del Toro',
+    year: 2006,
+}]
+
+fantasy = 'Pans Labyrinth'
+
+if (fantasy == 'Pans Labyrinth'){
+    console.log("MY FAVORIT MOVIE IS")
+} else {
+    console.log("NOT EVEN 5 STARS")
+}
